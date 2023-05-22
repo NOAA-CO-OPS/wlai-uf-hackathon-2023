393ac06e8a95bbc9cce239d0b616f79e857773a0
Evan Krell <ekrell@islander.tamucc.edu>
2023-05-22 10:52:04 -0500

    Added comments to

--- create_runs.py
+++ create_runs.py
@@ -6,6 +6,14 @@ from optparse import OptionParser
 import itertools
 
 def create_command(station_ids, name, data_dir, output_dir, hyperparams):
+  # Returns a string containing a python command to train the model
+  # Options:
+  #   station_ids: list of station IDs
+  #   name: arbitrary name to be added to the output files for identification
+  #   data_dir: path to directory with station data
+  #   output_dir: path to directory to save output files
+  #   hyperparams: string containing additional hyperparams (e.g. '-e 10 -r 0.1')
+
   cmd_str = "python train.py " + \
             " -s " + str(station_ids) + \
             " -m " + "model_" + str(name) + ".hdf5" + \
@@ -40,6 +48,8 @@ group_columns = options.columns.split(",")
 output_dir = options.output_dir
 data_dir = options.data_dir
 
+# Create a dictionary of hyperparamers
+# Only add hyperparams that are provided (otherwise, will use default values in `train.py`)
 hyperparams = {}
 if options.epochs is not None:
   hyperparams["-e"] = np.array(options.epochs.split(",")).astype("int")
@@ -48,41 +58,47 @@ if options.batch_size is not None:
 if options.resample_minority_percent is not None:
   hyperparams["-r"] = np.array(options.resample_minority_percent.split(",")).astype("float")
 
+# Load stations table
 dfStations = pd.read_csv(stations_file)
-station_ids = dfStations["station_id"].astype("string")
+# Get list of unique station IDs
+station_ids = np.unique(dfStations["station_id"].astype("string"))
 
+# Build list of strings for all combinations of hyperparams to sweep through
 hyperparam_combos = [[hpk + " " + str(v) for v in hyperparams[hpk]] for hpk in hyperparams.keys()]
 hyperparam_combos = list(itertools.product(*hyperparam_combos))
 hyperparam_combos = [" ".join(hpc) for hpc in hyperparam_combos]
 
+# For each column name provided, combine the stations and print the training run commands
 for group_column in group_columns:
 
   # Special case: column name 'ALL' is not a column. Instead, use all stations.
   if group_column == "ALL":
-    station_ids = np.unique(dfStations["station_id"].astype("string"))
+    # Use all station IDs
     station_ids_str = ",".join(station_ids)
-
+    # Sweep over hyperparameters for all stations
     for hyperparam_combo in hyperparam_combos:
       cmd_str = create_command(station_ids_str, "all-stations", data_dir, output_dir, hyperparam_combo)
-
+      # Print the training Python command
       print(cmd_str)
-
+    # Continue to next column name
     continue
 
   # Normal case: use the column name to group stations IDs by that column value (e.g. into geographic regions)
   try:
+    # Get all the values within that column (to be treated as station groups)
     groups = dfStations[group_column]
   except KeyError:
     print("Could not find column '{}' in stations file '{}'\nExiting...".format(group_column, stations_file))
     exit(-2)
 
+  # Combine the stations within each group
   for group in groups:
- 
+    # Subset the stations by group 
     dfStationsGroup = dfStations[dfStations[group_column] == group]
     group_station_ids = np.unique(dfStationsGroup["station_id"].astype("string"))
     group_station_ids_str = ",".join(group_station_ids)
-
+    # Sweep over hyperparameters for stations in this group
     for hyperparam_combo in hyperparam_combos:
       cmd_str = create_command(group_station_ids_str, group, data_dir, output_dir, hyperparam_combo)
-
+      # Print the training Python command
       print(cmd_str)
